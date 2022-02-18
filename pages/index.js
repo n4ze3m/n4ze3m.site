cc5b534f8a4d9f14401ee9d7d6cac1c1f988b9a4
nazeem <nazeemnob18@gmail.com>
2022-02-18 14:22:47 +0530

    Some changes

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ export default function Home() {
         <div className="flex flex-col-reverse sm:flex-row items-start">
           <div className="flex flex-col pr-8">
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black">
-              Muhammed Nazeem
+              {"Muhammed Nazeem"}
             </h1>
             <p className="text-md text-gray-500 mb-16">
-              I am a full stack developer. My skills lie mainly in the backend, but I am also competent in frontend development. I am passionate about programming and love to solve problems. I have experience with a variety of programming languages and frameworks, and I'm always up for learning new.
+              {"I am a full stack developer. My skills lie mainly in the backend, but I am also competent in frontend development. I am passionate about programming and love to solve problems. I have experience with a variety of programming languages and frameworks, and I'm always up for learning new."}
             </p>
           </div>
           <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">

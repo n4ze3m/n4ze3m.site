f0c91b1630a01fca4709fcb3c7d31dc4975bfe74
nazeem <nazeemnob18@gmail.com>
2022-04-25 15:27:42 +0530

    rickroll

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+export default function Page() {
+    return <div  className="p-6 m-auto">
+        <iframe src='https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1'
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+             height="315" 
+        />
+        <h1 className="text-center mt-6 text-2xl font-bold">
+            404
+        </h1>
+    </div>;
+}
